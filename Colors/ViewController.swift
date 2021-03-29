c26c7b8c7e486625455149db657dd82c7c980251
dcodocdcodco <snoozelight0@gmail.com>
2021-03-29 05:23:08 +0300

    without combine all colours

--- Colors/ViewController.swift
+++ Colors/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var redSliderValue: UILabel!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var greenSliderValue: UILabel!
+    
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var blueSliderValue: UILabel!
+    
+    @IBOutlet weak var colourView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSliderValue.text = String(redSlider.value)
+        redSlider.minimumTrackTintColor = .red
+        
+        greenSliderValue.text = String(greenSlider.value)
+        greenSlider.minimumTrackTintColor = .green
+        
+        blueSliderValue.text = String(blueSlider.value)
+        blueSlider.minimumTrackTintColor = .blue
     }
 
-
+    @IBAction func redSliderAction() {
+        redSliderValue.text = String(format: "%.1f", redSlider.value)
+        let sliderValue = CGFloat(redSlider.value)
+        colourView.backgroundColor = .red
+        colourView.backgroundColor = colourView.backgroundColor?.withAlphaComponent(sliderValue)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenSliderValue.text = String(format: "%.1f", greenSlider.value)
+        let sliderValue = CGFloat(greenSlider.value)
+        colourView.backgroundColor = .green
+        colourView.backgroundColor = colourView.backgroundColor?.withAlphaComponent(sliderValue)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueSliderValue.text = String(format: "%.1f", blueSlider.value)
+        let sliderValue = CGFloat(blueSlider.value)
+        colourView.backgroundColor = .blue
+        colourView.backgroundColor = colourView.backgroundColor?.withAlphaComponent(sliderValue)
+    }
 }
 
